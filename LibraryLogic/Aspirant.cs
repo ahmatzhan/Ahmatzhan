32612236f695b12c1be1dd0c65522bc4d4620b82
ahmatzhan <rakhmatzhanov@internet.ru>
2022-01-13 06:31:05 +0600

    add logic

---
+++ LibraryLogic/Aspirant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryLogic
+{
+    class Aspirant : UserLibrary
+    {
+        public Aspirant() : base()
+        {
+            Name = "Аспирант";
+            Rating = 15;
+        }
+        public Aspirant(string name, int rating) : base(name, rating)
+        {
+
+        }
+    }
+}
---
+++ LibraryLogic/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryLogic
+{
+    class Student : UserLibrary
+    {
+        public Student():base()
+        {
+            Name = "Студент";
+            Rating = 10;
+        }
+        public Student(string name, int rating) : base(name,rating)
+        {
+            
+        }
+        
+    }
+}
