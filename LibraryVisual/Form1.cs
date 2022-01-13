1e0683647189bf76e97cb5ae55e7cf615615ac1f
ahmatzhan <rakhmatzhanov@internet.ru>
2022-01-13 07:02:51 +0600

    add visual

---
+++ LibraryVisual/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryVisual
+{
+    public partial class LibraryForm : Form
+    {
+        public LibraryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBookButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RemoveBookButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
